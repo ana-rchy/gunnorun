5b98fb2c588752eaf2f05f7fcfc595ae5e50f0c1
ana_rchy <hmmmtesthmmm2@gmail.com>
2023-11-18 01:31:07 +0000

    idk

--- scripts/networking/LobbyManager.cs
+++ scripts/networking/LobbyManager.cs
@@ -3,9 +3,6 @@ using Godot;
 using static Godot.MultiplayerApi;
 
 public partial class LobbyManager : Node {
-    [Export(PropertyHint.Dir)] string _worldsDir;
-    [Export] PlayerManager _playerManager;
-
     public override void _Ready() {
         Paths.AddNodePath("LOBBY_MANAGER", GetPath());
     }
@@ -14,15 +11,6 @@ public partial class LobbyManager : Node {
     #region | rpc
 
     [Rpc(RpcMode.AnyPeer)] void Server_UpdateStatus(bool ready) {}
-
-    [Rpc] void Client_StartGame(string worldName) {
-        Global.PlayerData.ReadyStatus = false;
-        GetTree().ChangeSceneToFile($"{_worldsDir}/{worldName}.tscn");
-
-        foreach (var player in Global.OtherPlayerData) {
-            _playerManager.CallDeferred("CreateNewPuppetPlayer", player.Key, player.Value.Username, player.Value.Color);
-        }
-    }
 
     [Rpc] void Client_UpdateStatus(long id, bool ready) {
         if (Multiplayer.GetUniqueId() != id) {
--- scripts/networking/MatchManager.cs
+++ scripts/networking/MatchManager.cs
@@ -5,20 +5,19 @@ public partial class MatchManager : Node {
     [Export(PropertyHint.Dir)] string _worldDir;
     [Export] PlayerManager _playerManager;
 
-    [Rpc] void Client_PlayerWon(long id, double time, string nextWorld) {
+    [Rpc] void Client_LoadWorld(string worldName) {
+        Global.PlayerData.ReadyStatus = false;
+        GetTree().ChangeSceneToFile($"{_worldDir}/{worldName}.tscn");
+
+        foreach (var player in Global.OtherPlayerData) {
+            _playerManager.CallDeferred("CreateNewPuppetPlayer", player.Key, player.Value.Username, player.Value.Color);
+        }
+    }
+
+    [Rpc] void Client_PlayerWon(long id, double time) {
         var name = Multiplayer.GetUniqueId() == id ? Global.PlayerData.Username : Global.OtherPlayerData[id].Username;
         
         var playerUI = GetNode<PlayerUI>($"{Paths.GetNodePath("WORLD")}/{Multiplayer.GetUniqueId()}/PlayerUI");
         playerUI._OnRaceFinished((float) Math.Round(time, 3), name);
-
-        var timer = this.GetNodeConst<Timer>("FINISH_TIMER");
-        timer.Timeout += () => {
-            GetTree().ChangeSceneToFile($"{_worldDir}/{nextWorld}.tscn");
-            foreach (var player in Global.OtherPlayerData) {
-                _playerManager.CallDeferred("CreateNewPuppetPlayer", player.Key, player.Value.Username, player.Value.Color);
-            }
-        };
-        
-        this.GetNodeConst<Timer>("FINISH_TIMER").Start(); // needed so that timer doesnt continue after scene change
     }
 }
