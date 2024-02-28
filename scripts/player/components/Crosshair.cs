dbc27ddad6e3e8977dccc5ec49eaee4dc9a5716e
ana_rchy <hmmmtesthmmm2@gmail.com>
2024-02-28 22:17:08 +0000

    reposition/resize cursor, fix alphaarena

--- scripts/player/components/Crosshair.cs
+++ scripts/player/components/Crosshair.cs
@@ -2,19 +2,23 @@ using Godot;
 using System;
 
 public partial class Crosshair : Node {
-    [Export] Color _modulateColor = new Color(255, 255, 255);
+    [Export] Color _modulate = new Color(255, 255, 255);
     [Export] SubViewport _unfocusedCursor, _focusedCursor;
+    Sprite2D _unfocusedSprite, _focusedSprite;
 
     public override void _Ready() {
-        _unfocusedCursor.GetNode<Sprite2D>("Sprite").Modulate = _modulateColor;
-        _focusedCursor.GetNode<Sprite2D>("Sprite").Modulate = _modulateColor;
+        _unfocusedSprite = _unfocusedCursor.GetNode<Sprite2D>("Sprite");
+        _focusedSprite = _focusedCursor.GetNode<Sprite2D>("Sprite");
+
+        _unfocusedSprite.Modulate = _modulate;
+        _focusedSprite.Modulate = _modulate;
     }
 
     public override void _Process(double _) {
         if (Input.IsActionPressed("Shoot")) {
-            Input.SetCustomMouseCursor(_focusedCursor.GetTexture());
+            Input.SetCustomMouseCursor(_focusedCursor.GetTexture(), 0, _unfocusedSprite.Position);
         } else {
-            Input.SetCustomMouseCursor(_unfocusedCursor.GetTexture());
+            Input.SetCustomMouseCursor(_unfocusedCursor.GetTexture(), 0, _focusedSprite.Position);
         }
     }
 
